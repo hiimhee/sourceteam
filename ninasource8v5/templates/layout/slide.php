f42d0b29c1c26e0bddbd77511359831f35f6986e
hiimhee <phuongpham0603.nina@gmail.com>
2023-02-02 09:12:56 +0700

    # Updated thumb slider

--- ninasource8v5/templates/layout/slide.php
+++ ninasource8v5/templates/layout/slide.php
@@ -7,7 +7,7 @@
             <?php foreach ($slider as $v) { ?>
                 <div class="slideshow-item" owl-item-animation>
                     <a class="slideshow-image" href="<?= $v['link'] ?>" target="_blank" title="<?= $v['name' . $lang] ?>">
-                        <?= $func->getImage(['class' => 'lazy w-100', 'sizes' => '1920x560x1', 'upload' => UPLOAD_PHOTO_L, 'image' => $v['photo'], 'alt' => $v['name' . $lang]]) ?>
+                        <?= $func->getImage(['class' => 'lazy w-100', 'sizes' => '1920x600x1', 'upload' => UPLOAD_PHOTO_L, 'image' => $v['photo'], 'alt' => $v['name' . $lang]]) ?>
                     </a>
                 </div>
             <?php } ?>
